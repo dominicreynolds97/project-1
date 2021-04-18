aad1cd9dcff8887ec6ada3a73fb65cee456f4130
domr97 <dominicrjreynolds@gmail.com>
2021-04-18 14:15:48 +0100

    Levels, rotation by walls fix,,next Tetromino display and other bug fixes

--- tetris.js
+++ tetris.js
@@ -1,6 +1,8 @@
 const elements = {
   gameScreen: document.querySelector('#game-screen'),
   score: document.querySelector('#score'),
+  level: document.querySelector('#level'),
+  next: document.querySelector('#next-tetromino'),
 }
 
 class Tetromino {
@@ -9,20 +11,33 @@ class Tetromino {
     this.shape = shape
     this.color = color
     this.x = 3
-    this.y = 0
+    this.y = 0 - this.width
   }
 
   display() {
     this.shape.forEach((e, i) => e.forEach((e2, j) => {
-      if (e2) {
+      if (e2 && (this.y + i >= 0)) {
         cells[this.x + j][this.y + i].color = this.color
         cells[this.x + j][this.y + i].div.style = 'background-color: ' + this.color
-      } else if (cells[this.x + j] && !cells[this.x + j][this.y + i].hasTetromino) {
+      } else if (!(this.y + i > 19) && (this.y + i >= 0) && cells[this.x + j] && !cells[this.x + j][this.y + i].hasTetromino) {
         cells[this.x + j][this.y + i].div.style = 'background-color: black'
       }
     }))
   }
   
+  displayNext() {
+    for (let x = 0; x < 4; x++) {
+      for (let y = 0; y < 4; y++) {
+        if (x < this.width && y < this.width && this.shape[x][y]) {
+          nextCells[x][y].color = this.color
+          nextCells[x][y].div.style = 'background-color: ' + this.color
+        } else {
+          nextCells[x][y].div.style = 'background-color: black'
+        }
+      }
+    }
+  }
+
   drop() {
     if (this.collisionDetection()) {
       return true
@@ -30,7 +45,7 @@ class Tetromino {
     this.shape.forEach((e, i) => e.forEach((e2, j) => {
       if (!e2 && (!cells[this.x + j])) {
         console.log(cells[this.x + j])
-      } else if (!cells[this.x + j][this.y + i].hasTetromino) {
+      } else if ((this.y + i >= 0) && !cells[this.x + j][this.y + i].hasTetromino) {
         cells[this.x + j][this.y + i].div.style = 'background-color: black'
       }
     }))
@@ -47,7 +62,6 @@ class Tetromino {
         canMove = false
       } 
     }))
-    console.log(canMove)
     if (canMove) {
       this.shape.forEach((e, i) => e.forEach((e2, j) => {
         if (cells[this.x + j] && !cells[this.x + j][this.y + i].hasTetromino) {
@@ -66,10 +80,20 @@ class Tetromino {
     }
     if (isClockwise) {
       this.shape.forEach((e, i) => e.forEach((e2, j) => {
+        if (i + this.x >= 10) {
+          this.moveSideways(-1)
+        } else if (i + this.x <= 0) {
+          this.moveSideways(1)
+        }
         newShape[j].unshift(this.shape[i][j])
       }))
     } else {
       this.shape.forEach((e, i) => e.forEach((e2, j) => {
+        if (i + this.x >= 10) {
+          this.moveSideways(-1)
+        } else if (i + this.x < 0) {
+          this.moveSideways(1)
+        }
         newShape[this.width - 1 - j].push(this.shape[i][j])
       }))
     }
@@ -80,7 +104,7 @@ class Tetromino {
     let hasCollided = false
     this.shape.forEach((e, i) => {
       e.forEach((e2, j) => {
-        if (e2 === true && (this.y + i === 19 || cells[this.x + j][this.y + i + 1].hasTetromino)) {
+        if (e2 === true && this.y + i > 0 && (this.y + i === 19 || cells[this.x + j][this.y + i + 1].hasTetromino)) {
           hasCollided = true
         }
       })
@@ -110,8 +134,10 @@ const tetrominoChoices = [
 ]
 
 const cells = []
-const round = 1
+const nextCells = []
+let level = 0
 let score = 0
+let linesCleared = 0
 
 for (let x = 0; x < 10; x++) {
   const tempArray = []
@@ -124,29 +150,45 @@ for (let x = 0; x < 10; x++) {
       div: cellDiv,
       color: 'black',
     }
-    cellDiv.addEventListener('click', () => {
-      console.dir(cell)
-    })
     tempArray.push(cell)
   }
   cells.push(tempArray)
 }
 
-let num = Math.floor(Math.random() * 7)
-const tetrominos = [new Tetromino(tetrominoChoices[num].shape, tetrominoChoices[num].color)]
+for (let x = 0; x < 4; x++) {
+  const tempArray = []
+  for (let y = 0; y < 4; y++) {
+    const cellDiv = document.createElement('div')
+    cellDiv.classList.add('next-cell')
+    elements.next.appendChild(cellDiv)
+    const cell = {
+      div: cellDiv,
+      color: 'black',
+    }
+    tempArray.push(cell)
+  }
+  nextCells.push(tempArray)
+}
+
+let r = Math.floor(Math.random() * 7)
+const r2 = Math.floor(Math.random() * 7)
+const tetrominos = [new Tetromino(tetrominoChoices[r].shape, tetrominoChoices[r].color), new Tetromino(tetrominoChoices[r2].shape, tetrominoChoices[r2].color)]
 let currentTetromino = 0
 
-
-setInterval(() => {
+function playGame() {
   const newTet = tetrominos[currentTetromino].drop()
   if (newTet) {
+    r = Math.floor(Math.random() * 7)
+    tetrominos.push(new Tetromino(tetrominoChoices[r].shape, tetrominoChoices[r].color))
     checkTetris()
-    num = Math.floor(Math.random() * 7)
-    tetrominos.push(new Tetromino(tetrominoChoices[num].shape, tetrominoChoices[num].color))
-    
     currentTetromino++
   }
-}, 500)
+  tetrominos[currentTetromino + 1].displayNext()
+  setTimeout(() => {
+    playGame()
+  }, 500)
+}
+
 
 document.addEventListener('keydown', e => {
   const key = e.key
@@ -178,12 +220,7 @@ function checkTetris() {
       }
     } 
     if (row.every(e => e === true)) {
-      console.log('tetris')
       tetrisCount++
-      // cells.forEach((e, i) =>  {
-      //   cells[i][y].div.style = 'background-color: black'
-      //   cells[i][y].hasTetromino = false
-      // })
       removeRow(y)
     }
   }
@@ -193,18 +230,11 @@ function checkTetris() {
     case 3: updateScore(300); break
     case 4: updateScore(1200); break
   }
+  linesCleared += tetrisCount
+  levelCheck()
 }
 
 function removeRow(y) {
-  // const pivotedArray = []
-  // for (let i = 0; i < 20; i++) {
-  //   pivotedArray.push([])
-  // }
-  // cells.forEach((e) => {
-  //   e.forEach((e2, j) => {
-  //     pivotedArray[j].push(e2)
-  //   })
-  // })
   for (let x = 0; x < 10; x++) {
     for (let i = y; i > 0; i--) {
       const tempCell = cells[x][i - 1]
@@ -218,24 +248,20 @@ function removeRow(y) {
       }
     }
   }
-  console.dir(cells)
-  // pivotedArray.splice(y, 1)
-  // pivotedArray.unshift(newRow)
-  // const newCells = []
-  // for (let i = 0; i < 10; i++) {
-  //   newCells.push([])
-  // }
-  // pivotedArray.forEach((e, i) => {
-  //   e.forEach((e2, j) => {
-  //     cells[j][i] = e2
-  //   })
-    
-  // })
-  // console.dir(newCells)
-  // cells = newCells
 }
 
 function updateScore(points) {
-  score += round * points
+  score += (level + 1) * points
   elements.score.innerHTML = score
 }
+
+function levelCheck() {
+  console.log(linesCleared)
+  if (linesCleared >= ((level + 1) * 10)) {
+    level++
+    linesCleared -= (level * 10)
+    elements.level.innerHTML = level
+  }
+}
+
+playGame()
